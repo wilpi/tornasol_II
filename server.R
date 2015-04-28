03b28f2f7ec650c8ca852531c46df1a7597e613c
Wilmer Fuentes Neira <elhombrepi@hotmail.com>
2015-04-28 14:33:42 -0500

    336 posibles graficas

--- server.R
+++ server.R
@@ -11,7 +11,7 @@ shinyServer(function(input, output) {
 
 et<-matrix(c("Hipertenso", "No Hipertenso","Dislipidemico", 
                  "No Dislipidemico","Diabetico", "No Diabetico",
-                 "Fumador", "No Fumador","Obeso", "No obeso"),2,5)
+                 "Fumador", "No Fumador","Obeso", "No obeso","Sedentario","No sedentario"),2,6)
     
     
     blank_theme <- theme_minimal()+
@@ -32,12 +32,12 @@ et<-matrix(c("Hipertenso", "No Hipertenso","Dislipidemico",
         etiqet <-c(input2)        
         #Va INPUT2 ingresar como texto y se convertido a numero.
         input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
-                        'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5")
+                        'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
        
         input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
         
         set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
-                      3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100'")
+                      3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100';6='sedentarismo'")
                 
         if(input33==0){
             midata<-subset(au, estudio==set1)
@@ -70,12 +70,12 @@ pv3<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
     etiqet <-c(input2)        
     #Va INPUT2 ingresar como texto y se convertido a numero.
     input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
-                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5")
+                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
     
     input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
     
     set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
-                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100'")
+                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100';6='sedentarismo'")
     
     if(input33==0){
         midata<-subset(au, estudio==set1)
@@ -97,8 +97,183 @@ pv3<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
     return(ploty)
 }
 
+pv4<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
+    #"Tornasol I"
+    #"set1 suppor the subsetof midata     
+    set1 <-input1
+    #Va entrar puro
+    etiqet <-c(input2)        
+    #Va INPUT2 ingresar como texto y se convertido a numero.
+    input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
+                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
+    
+    input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
+    
+    set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
+                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100';6='sedentarismo'")
+    
+    if(input33==0){
+        midata<-subset(au, estudio==set1)
+    }
+    else {
+        midata<-subset(au, estudio==set1 & region==input3)   
+    }
+    incidents<-aggregate(midata[,set2], list(midata$sexo), mean,na.rm=TRUE)
+    
+    incidents$x<-round(incidents$x,1)    
+    ploty<-ggplot(data=incidents, aes(x=Group.1, y=x)) +
+        geom_bar(stat="identity", fill=c("#E69F00", "#0072B2"), width=0.75)+ xlab("")+
+        ylab("Prevalencia (%)") + ggtitle("Prevalencia por sexo")+
+        geom_text(aes(label=prettyNum(x, big.mark=",")), vjust=4.7, color="white")+
+        scale_y_continuous(expand = c(0, 1))+
+        theme(axis.text=element_text(size=14), 
+              axis.title=element_text(size=14,face="bold"))
+    
+    return(ploty)
+}
 
-   
+pv5<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
+    #"Tornasol I"
+    #"set1 suppor the subsetof midata     
+    set1 <-input1
+    #Va entrar puro
+    etiqet <-c(input2)        
+    #Va INPUT2 ingresar como texto y se convertido a numero.
+    input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
+                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
+    
+    input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
+    
+    set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
+                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100'; 6='sedentarismo'")
+    
+    if(input33==0){
+        midata<-subset(au, estudio==set1)
+    }
+    else {
+        midata<-subset(au, estudio==set1 & region==input3)   
+    }
+    incidents<-aggregate(midata[,set2], list(midata$ciudad2), mean)
+       
+    incidents$x<-round(incidents$x,1)    
+    ploty<-ggplot(data=incidents, aes(x=Group.1, y=x)) +
+        geom_bar(stat="identity", fill="lightblue", width=0.75)+ xlab("")+
+        ylab("Prevalencia (%)") + ggtitle("Prevalencia por ciudad")+
+        geom_text(aes(label=prettyNum(x, big.mark=",")), vjust=3, color="black", size=5)+
+        scale_y_continuous(expand = c(0, 1))+
+        theme_bw() + theme(axis.title.x = element_text(face="bold", colour="#990000", size=4),
+                           axis.text.x  = element_text(angle=45,hjust=1,vjust=1.1, size=13))
+    return(ploty)
+}
+
+
+pv6<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
+    #"Tornasol I"
+    #"set1 suppor the subsetof midata     
+    set1 <-input1
+    #Va entrar puro
+    etiqet <-c(input2)        
+    #Va INPUT2 ingresar como texto y se convertido a numero.
+    input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
+                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
+    
+    input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
+    
+    set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
+                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100';6='sedentarismo'")
+    
+    if(input33==0){
+        midata<-subset(au, estudio==set1)
+    }
+    else {
+        midata<-subset(au, estudio==set1 & region==input3)   
+    }
+    incidents<-aggregate(midata[,set2], list(midata$EDUCACION), mean,na.rm=TRUE)
+    
+    incidents$x<-round(incidents$x,1)    
+    
+    
+    ploty<-ggplot(data=incidents, aes(x=Group.1, y=x)) +
+        geom_bar(stat="identity", fill=c("#999999", "#E69F00", "#56B4E9", "#009E73"), width=0.75)+ xlab("")+
+        ylab("Prevalencia (%)") + ggtitle("Prevalencia por nivel educativo")+
+        geom_text(aes(label=prettyNum(x, big.mark=",")), vjust=2, color="white")+
+        scale_y_continuous(expand = c(0, 1))+
+        theme(axis.text.x  = element_text(angle=45,hjust=1,vjust=1.1, size=13))
+    
+    return(ploty)
+}
+
+
+pv7<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
+    #"Tornasol I"
+    #"set1 suppor the subsetof midata     
+    set1 <-input1
+    #Va entrar puro
+    etiqet <-c(input2)        
+    #Va INPUT2 ingresar como texto y se convertido a numero.
+    input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
+                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
+    
+    input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
+    
+    set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
+                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100';6='sedentarismo'")
+    
+    if(input33==0){
+        midata<-subset(au, estudio==set1)
+    }
+    else {
+        midata<-subset(au, estudio==set1 & region==input3)   
+    }
+    incidents<-aggregate(midata[,set2], list(midata$clase_soc), mean,na.rm=TRUE)
+    
+    incidents$x<-round(incidents$x,1)    
+    
+    
+    ploty<-ggplot(data=incidents, aes(x=Group.1, y=x)) +
+        geom_bar(stat="identity", fill=c("#F0E442", "#0072B2", "#D55E00"), width=0.75)+ xlab("")+
+        ylab("Prevalencia (%)") + ggtitle("Prevalencia por nivel educativo")+
+        geom_text(aes(label=prettyNum(x, big.mark=",")), vjust=2, color="white")+
+        scale_y_continuous(expand = c(0, 1))+
+        theme(axis.text.x  = element_text(hjust=1,vjust=1.1, size=13))
+    
+    return(ploty)
+}
+
+pv8<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
+    #"Tornasol I"
+    #"set1 suppor the subsetof midata     
+    set1 <-input1
+    #Va entrar puro
+    etiqet <-c(input2)        
+    #Va INPUT2 ingresar como texto y se convertido a numero.
+    input22 <-recode(input2, "'Hipertension'=1; 'Dislipidemia'=2;
+                     'Diabetes'=3 ;'Tabaquismo'=4;'Obesidad'=5;'Sedentarismo'=6")
+    
+    input33<-recode(input3,"'Nacional'=0;'Costa'=1;'Sierra'=2;'Selva'=3")
+    
+    set2 <-recode(input22, "1='htaxxx_x_100'; 2='cholestt_dico_x_100';
+                  3='diabet_dico_x_100';4='fums_dico_x_100';5='imc_dico_x_100';6='sedentarismo'")
+    
+    if(input33==0){
+        midata<-subset(au, estudio==set1)
+    }
+    else {
+        midata<-subset(au, estudio==set1 & region==input3)   
+    }
+    incidents<-aggregate(midata[,set2], list(midata$etario,midata$sexo), mean,na.rm=TRUE)
+    
+   ploty<-ggplot(data=incidents, aes(x=Group.1, y=x, group=Group.2, colour=Group.2)) +
+        geom_line() + geom_point()+xlab("")+
+        ylab("Prevalencia (%)") + ggtitle("Prevalencia por edad y sexo")+
+        scale_y_continuous(breaks = round(seq(min(0), max(incidents$x), by = 5),1))+
+        theme_bw()
+    
+    return(ploty)
+}
+
+
+
     dataInput <- reactive({
         pv2(c(input$dataset),c(input$pato),c(input$region)  
             #input$region)                   
@@ -109,7 +284,41 @@ pv3<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
      pv3(c(input$dataset),c(input$pato),c(input$region)  
         #input$region)                   
      )    
-})
+    })
+
+    dataInput3 <- reactive({
+    pv4(c(input$dataset),c(input$pato),c(input$region)  
+        #input$region)                   
+    )    
+    })
+
+    dataInput5 <- reactive({
+    pv6(c(input$dataset),c(input$pato),c(input$region)  
+        #input$region)                   
+    )    
+    })
+
+
+    dataInput6 <- reactive({
+    pv7(c(input$dataset),c(input$pato),c(input$region)  
+        #input$region)                   
+    )    
+    })
+
+    dataInput7 <- reactive({
+    pv8(c(input$dataset),c(input$pato),c(input$region)  
+                           
+    )    
+    })
+
+
+dataInput4 <- reactive({
+    pv5(c(input$dataset),c(input$pato),c(input$region)  
+        #input$region)                   
+    )    
+    })
+
+
 #HELP ME HELP ME me falta hacer pestañas para multiple outpu
     output$plot <- renderPlot({    
                 (dataInput())      
@@ -118,5 +327,24 @@ pv3<-function(input1="Tornasol I",input2="Hipertension",input3="Nacional"){
     output$plut <- renderPlot({    
                 (dataInput2())      
                              })
+    output$plit <- renderPlot({    
+    (dataInput4())      
+    })
 
+    output$mabel <- renderPlot({    
+    (dataInput5())      
+    })
+    output$ysamar <- renderPlot({    
+    (dataInput6())      
+    })
+    output$francis <- renderPlot({    
+    (dataInput7())      
+    })
+
+    output$plat <- renderPlot({    
+    (dataInput3())      
+                            })
 })
+
+
+
--- ui.R
+++ ui.R
@@ -19,7 +19,7 @@ shinyUI(fluidPage(
             selectInput("dataset", label = "Choose a Study:", 
                         choices = c("Tornasol I","Tornasol II")),
             selectInput("pato", label = "Choose a Disease:", 
-                        choices = c("Hipertension","Dislipidemia","Diabetes","Tabaquismo","Obesidad")),
+                        choices = c("Hipertension","Dislipidemia","Diabetes","Tabaquismo","Obesidad", "Sedentarismo")),
             selectInput("region",label =  "Choose a Region:", 
                         choices = c("Nacional","Costa","Sierra","Selva")),
                     
@@ -30,8 +30,21 @@ shinyUI(fluidPage(
         mainPanel(
             tabsetPanel(type = "tabs", 
                         tabPanel("General", plotOutput("plot")),
-                        tabPanel("Por Sexo", "En construccion" ),
-                        tabPanel("Grupo de Edad", plotOutput("plut") )
+                        tabPanel("Sex", plotOutput("plat") ),
+                        tabPanel("Age Group", plotOutput("plut")),
+                        tabPanel("City", plotOutput("plit")),
+                        tabPanel("Education", plotOutput("mabel")),
+                        tabPanel("Socioeconomic", plotOutput("ysamar")),
+                        tabPanel("Sex vs Age", plotOutput("francis")),
+                        tabPanel("Credits",                  
+                                h4('The app is intended to show a prediction for a childs height based on the heights of the parents.'),
+                                 p('The user needs to add in the height of the father and height of the mother and check the box for
+                                if they want to know the predicted height of a son, a daughter or both. The app will run a simple
+                                calculation (note. this is not based of any real data of childrens heights but just a simple sum
+                                to shows the functions are working) and show the average expected height of a child. If the check
+                                boxes are ticked a calculation for a son and/or a daughter will also appear'),
+                                 p('The ui.R and server.R files can be found at the following github site: https://github.com/BDFace/DDP_Project_Shiny')
+                           )
                                    
             # h2("Prevalence of Cardiovascular Risk Factor"),
             #h5("Draft of work - i have a problem to pass ALL argument(diseases,region) to my function plot pv2, pv2 work fine out of shiny  "),
